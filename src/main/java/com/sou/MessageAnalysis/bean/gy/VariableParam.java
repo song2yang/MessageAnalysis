ff956f032eaaf3bce656f245b67087516c167d76
souyouyou <im.songyangyang@gmail.com>
2018-12-18 15:51:24 +0800

    left_outer

---
+++ src/main/java/com/sou/MessageAnalysis/bean/gy/VariableParam.java
@@ -0,0 +1,23 @@
+package com.sou.MessageAnalysis.bean.gy;
+
+public class VariableParam {
+    private Integer days;
+    private String lableName;
+
+
+    public Integer getDays() {
+        return days;
+    }
+
+    public void setDays(Integer days) {
+        this.days = days;
+    }
+
+    public String getLableName() {
+        return lableName;
+    }
+
+    public void setLableName(String lableName) {
+        this.lableName = lableName;
+    }
+}
